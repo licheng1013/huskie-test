251f25c01875ff2188d26c57046bac8f98d96d9e
licheng10113 <2501093733@qq.com>
2023-03-08 22:56:20 +0800

    暂时移除不能使用的api!

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -11,16 +11,16 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-#[tauri::command]
-fn request_test(request: &Request)  {
-    println!("请求地址{:?}",request.request_addr)
-}
+// #[tauri::command]
+// fn request_test(request: &Request)  {
+//     println!("请求地址{:?}",request.request_addr)
+// }
 
 
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
-        .invoke_handler(tauri::generate_handler![request_test])
+        //.invoke_handler(tauri::generate_handler![request_test])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
