682e7da5d366a7bb6ac3a0516a26ae627e6485bb
licheng1013 <2501093733@qq.com>
2023-03-09 14:39:19 +0800

    增加线程循环工具!

--- src-tauri/src/util/thread_util.rs
+++ src-tauri/src/util/thread_util.rs
@@ -3,32 +3,47 @@ use std::thread;
 pub struct ThreadUtil {}
 
 impl ThreadUtil {
-    fn run(f: Box<dyn Fn() + Send + 'static>) {
+    // impl是编译的时候内存就固定了的，dyn是不固定的
+    fn run(f: impl Fn() + Send + 'static) {
         //f(); // 这行移除注释会报错,因为rust不允许调用后,再次移动闭包
-        let handle = thread::spawn(move || {
+        let _handle = thread::spawn(move || {
             f();
         });
-        handle.join().unwrap()
     }
-    fn many_run(threadCount: i32, f: Box<dyn Fn() + Send + 'static>) {
-        ThreadUtil::run(f);
-        //for i in 0..threadCount {
-        //}
+    pub fn many_run(thread_count: i32,f: impl Fn() + Send  + 'static + Clone) {
+        for _i in 0..thread_count {
+            ThreadUtil::run(f.clone());
+        }
     }
 
 }
 
 #[cfg(test)]
 mod test {
+    use std::{thread, time::{Duration, SystemTime}, sync::mpsc};
+
     use super::ThreadUtil;
 
     #[test]
     fn test() {
-        let f = Box::new(|| {
-            for i in 1..10000 {
-                println!("HelloWorld: {:?}", i)
+        let thread_count = 30;
+        let count = 10000;
+        let (tx, rx) = mpsc::channel();
+        let f = move|| {
+            for _i in 0..count {
+                //println!("HelloWorld: {:?}", i);
+                tx.send(1).unwrap();
             }
-        });
-        ThreadUtil::many_run(10,f);
+        };
+        ThreadUtil::many_run(thread_count,f);
+        let start_time = SystemTime::now();
+        let mut number  = 0;
+        for _i in 0..count*thread_count{
+            let received = rx.recv().unwrap();
+            number+=received;
+            println!("数据: {:?}",number)
+        }
+        let end_time = SystemTime::now();
+        println!("执行总耗时: {:?}",end_time.duration_since(start_time).unwrap().as_millis())
     }
 }
